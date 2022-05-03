f9b1e40d15c4373bf0e406cb51e671e4666e3357
Roman Voloshyn <rmnvlshn123@gmail.com>
2022-05-03 21:28:11 +0300

    Added nav_graph, setup firebase real database

---
+++ app/src/main/java/com/example/graduationproject/ui/IntroFragment.kt
@@ -0,0 +1,32 @@
+package com.example.graduationproject.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.graduationproject.R
+
+class IntroFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = IntroFragment()
+    }
+
+    private lateinit var viewModel: IntroViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.intro_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(IntroViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/graduationproject/ui/IntroViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.graduationproject.ui
+
+import androidx.lifecycle.ViewModel
+
+class IntroViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
